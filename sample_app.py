c9af2d5005114e14426325a855a95e92c5fd28ed
Mike Krieger <mikeyk@instagr.am>
2011-02-20 10:34:08 -0800

    adding commas as default to prevent confusion

--- sample_app.py
+++ sample_app.py
@@ -2,9 +2,9 @@ from bottle import route, run, request
 from instagram import client
 
 CONFIG = {
-    'client_id': ''
-    'client_secret': ''
-    'redirect_uri': 'http://localhost:8515/oauth_callback'
+    'client_id': '',
+    'client_secret': '',
+    'redirect_uri': ''
 }
 
 unauthenticated_api = client.InstagramAPI(**CONFIG)
@@ -12,25 +12,28 @@ unauthenticated_api = client.InstagramAPI(**CONFIG)
 @route('/')
 def home():
     try:
-        url = unauthenticated_api.get_authorize_url()
+        url = unauthenticated_api.get_authorize_url(scope=["likes","comments"])
         return '<a href="%s">Connect with Instagram</a>' % url
     except Exception, e:
-        return e
+        print e
 
 @route('/oauth_callback')
 def on_callback():
     code = request.GET.get("code")
     if not code:
         return 'Missing code'
-    access_token = unauthenticated_api.exchange_code_for_access_token(code)
-    if not access_token:
-        return 'Could not get access token'
-    
-    api = client.InstagramAPI(access_token=access_token)
-    recent_media, next = api.user_recent_media()
-    photos = []
-    for media in recent_media:
-        photos.append('<img src="%s"/>' % media.images['thumbnail'].url)
-    return ''.join(photos)
+    try:
+        access_token = unauthenticated_api.exchange_code_for_access_token(code)
+        if not access_token:
+            return 'Could not get access token'
+        
+        api = client.InstagramAPI(access_token=access_token)
+        recent_media, next = api.user_recent_media()
+        photos = []
+        for media in recent_media:
+            photos.append('<img src="%s"/>' % media.images['thumbnail'].url)
+        return ''.join(photos)
+    except Exception, e:
+        print e
 
 run(host='localhost', port=8515)
