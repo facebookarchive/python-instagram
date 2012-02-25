67a60c69e875a39eff0fec1d8698ed22df274e5a
Mike Krieger <mikeyk@instagram.com>
2012-02-24 16:18:39 -0800

    Another version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="python-instagram",
-      version="0.7.1",
+      version="0.7.2",
       description="Instagram API client",
       license="MIT",
       install_requires=["simplejson","httplib2"],
