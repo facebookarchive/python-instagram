21ba5cb51fdc2b24c2942a44be10138aa6327b49
Jon Heaton <jonh@fb.com>
2014-08-05 15:13:39 +0100

    version increase for 1.1.3 release;

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="python-instagram",
-      version="1.1.2",
+      version="1.1.3",
       description="Instagram API client",
       license="MIT",
       install_requires=["simplejson","httplib2"],
