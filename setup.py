2ba69fe81e5bf64c88c30d413209593721b2b3da
Josh Watts <josh.watts@shoutlet.com>
2014-09-25 17:44:06 -0400

    updated version to 1.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="python-instagram",
-      version="1.1.3",
+      version="1.1.4",
       description="Instagram API client",
       license="MIT",
       install_requires=["simplejson","httplib2"],
