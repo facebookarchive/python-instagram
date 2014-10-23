3e5ba60c1cdda3b8fd4f847f8972f98e79b8b1dc
Jon Heaton <jonh@fb.com>
2014-10-23 14:23:09 +0100

    Python 3 Compatibility

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="python-instagram",
-      version="1.1.4",
+      version="1.2.0",
       description="Instagram API client",
       license="MIT",
       install_requires=["simplejson","httplib2","six"],
